fd65019fb58295b3a74b66591e3a22db87e5f677
Kyle McClellan <kylemcclellan3@gmail.com>
2021-11-26 08:16:26 -0500

    Offset buffer implementation

--- Confluent.Kafka.Dataflow/Consuming/OffsetBuffer.cs
+++ Confluent.Kafka.Dataflow/Consuming/OffsetBuffer.cs
@@ -1,18 +1,37 @@
 namespace Confluent.Kafka.Dataflow.Consuming
 {
     using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
     using Confluent;
 
     class OffsetBuffer<TKey, TValue>
     {
+        readonly ConcurrentDictionary<Message<TKey, TValue>, TopicPartitionOffset> offsets = new();
+        readonly Dictionary<TopicPartition, Offset> positions = new();
+
         public void Add(Message<TKey, TValue> message, TopicPartitionOffset offset)
         {
-            throw new NotImplementedException();
+            if (!this.offsets.TryAdd(message, offset))
+            {
+                throw new InvalidOperationException("Message already added.");
+            }
         }
 
         public TopicPartitionOffset Retrieve(Message<TKey, TValue> message)
         {
-            throw new NotImplementedException();
+            if (!this.offsets.TryRemove(message, out var tpo))
+            {
+                throw new InvalidOperationException("No offset added for message.");
+            }
+
+            if (this.positions.TryGetValue(tpo.TopicPartition, out var previous) && previous > tpo.Offset)
+            {
+                throw new InvalidOperationException("Offsets retrieved out of order for partition.");
+            }
+
+            this.positions[tpo.TopicPartition] = tpo.Offset;
+            return tpo;
         }
     }
 }
